0f21fc32bc2c1776ae7326007ed1a6022643b9d3
EX_Rago, Giovanni (Heidelberg) DEU <giovanni.rago@saucelabs.com>
2017-06-02 15:44:28 +0200

    Less printouts...

--- src/test/java/com/testobject/BasicTestSetup.java
+++ src/test/java/com/testobject/BasicTestSetup.java
@@ -60,22 +60,22 @@ public class BasicTestSetup {
 
 		capabilities.setCapability("uuid", UUID.randomUUID().toString());
 
-		printWithTimestamp("Test setup starting, initialising driver with capabilities:");
-		System.out.println(capabilities.toString());
+//		printWithTimestamp("Test setup starting, initialising driver with capabilities:");
+//		System.out.println(capabilities.toString());
 
 		driver = new IOSDriver(new URL("https://eu1.appium.testobject.com/wd/hub"), capabilities);
 
-		printWithTimestamp("Session established.");
+//		printWithTimestamp("Session established.");
 
-		printWithTimestamp(driver.getCapabilities().getCapability("testobject_test_report_url").toString());
-		printWithTimestamp(driver.getCapabilities().getCapability("testobject_test_live_view_url").toString());
+//		printWithTimestamp(driver.getCapabilities().getCapability("testobject_test_report_url").toString());
+//		printWithTimestamp(driver.getCapabilities().getCapability("testobject_test_live_view_url").toString());
 
 	}
 
 	/* We disable the driver after EACH test has been executed. */
 	@After
 	public void tearDown() {
-		printWithTimestamp("Test ended, tearing down session.");
+//		printWithTimestamp("Test ended, tearing down session.");
 		driver.quit();
 		long endTime = new Date().getTime();
 		printWithTimestamp("Session closed, total test duration: " + (endTime - startTime)/1000 + "s");
@@ -84,9 +84,9 @@ public class BasicTestSetup {
 	/* A simple addition, it expects the correct result to appear in the result field. */
 	@Test
 	public void iosAllocationTest() {
-		printWithTimestamp("Test started, taking screenshot...");
+//		printWithTimestamp("Test started, taking screenshot...");
 		driver.getScreenshotAs(OutputType.BASE64);
-		printWithTimestamp("Getting page source (not printing to save room...)");
+//		printWithTimestamp("Getting page source (not printing to save room...)");
 		driver.getPageSource();
 
 	}
